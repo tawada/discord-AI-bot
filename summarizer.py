6e1a131a49a2dd8214b5faf6448207dc0006d1b3
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-04-18 01:36:04 +0000

    Set userAgent

--- summarizer.py
+++ summarizer.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def summarize_webpage(url, openai_client):
     # Get the webpage
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1"})
     logger.debug(response.status_code)
     logger.debug(response.text)
     html = response.text
