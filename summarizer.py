a95791c5474e4389bf8e675f3bc3defca4d81afc
tawada <tawada@togawa.cs.waseda.ac.jp>
2024-04-23 14:55:24 +0000

    Add function to summarize YouTube webpage titles

--- summarizer.py
+++ summarizer.py
@@ -6,21 +6,16 @@ logger = logging.getLogger(__name__)
 
 
 def summarize_webpage(url, openai_client):
+    """"""
+    if "youtube.com" in url:
+        return summarize_youtube(url, openai_client)
+
     # Get the webpage
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1"})
     logger.debug(response.status_code)
     logger.debug(response.text)
     html = response.text
     logger.debug(html[:50])
-
-    if url.startswith("https://youtube.com/") or url.startswith(
-        "https://www.youtube.com/"
-    ):
-        idx_s = html.find("<title>")
-        idx_e = html.find("</title>")
-        title = html[idx_s + 7 : idx_e]
-        logger.debug(f"Title: {title}")
-        return title
 
     # Limit the length of the HTML to 4096 characters
     html = html[:4096]
@@ -42,3 +37,17 @@ def summarize_webpage(url, openai_client):
         logger.exception(err)
         raise
     return summarized_text
+
+
+def summarize_youtube(url, openai_client):
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    logger.debug(response.status_code)
+    logger.debug(response.text)
+    html = response.text
+    logger.debug(html[:50])
+
+    idx_s = html.find("<title>")
+    idx_e = html.find("</title>")
+    title = html[idx_s + 7 : idx_e]
+    logger.debug(f"Title: {title}")
+    return "This youtube video is " + title
