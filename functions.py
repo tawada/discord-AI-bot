0abd4652cc3919ebbcf554fdd65024db99449646
tawada <tawada@togawa.cs.waseda.ac.jp>
2025-01-04 03:53:29 +0000

    Refactor URL regex to use precompiled pattern

--- functions.py
+++ functions.py
@@ -1,18 +1,27 @@
 import re
 
 
+url_pattern_raw = (
+    'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]'
+    '|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+)
+url_pattern = re.compile(url_pattern_raw)
+
+
 def has_url(text_including_url: str) -> bool:
     """Uses regex to check if a string has a URL."""
-    return bool(re.search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text_including_url))
+    return bool(re.search(url_pattern, text_including_url))
 
 
 def get_urls(text_including_url: str) -> list:
     """Uses regex to extract URLs from a string."""
-    return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text_including_url)
+    return re.findall(url_pattern, text_including_url)
 
 
 def is_knowledge_request(text: str) -> bool:
-    """Checks if a string contains any of the keywords for knowledge requests."""
+    """Checks if a string contains any of the
+       keywords for knowledge requests.
+    """
     keywords = ["教えて", "誰", "何", "調べて"]
     return any(keyword in text for keyword in keywords)
 
@@ -21,5 +30,6 @@ def is_image_attachment(attachment) -> bool:
     """Checks if an attachment is an image."""
     extensions = (".png", ".jpg", ".jpeg")
     return attachment.content_type.startswith("image") and (
-        attachment.filename.endswith(extensions) or attachment.url.endswith(extensions)
+        attachment.filename.endswith(extensions)
+        or attachment.url.endswith(extensions)
     )
